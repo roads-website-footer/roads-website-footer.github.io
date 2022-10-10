33e6e8d374e1bea9946d05e24ee0327c5cbfdee5
RoadsTechDev-1 <115484948+RoadsTechDev-1@users.noreply.github.com>
2022-10-10 17:17:17 +0200

    first commit DevAccount

--- element.js
+++ element.js
@@ -1,10 +1,26 @@
 customElements.define(
   "roads-website-footer",
   class extends HTMLElement {
+
     constructor() {
       super() // or this when done in the connectedCallback
         .attachShadow({ mode: "open" })
         .append(document.getElementById(this.nodeName).content.cloneNode(true));
     }
+
+    $(selector){
+      return [...this.shadowRoot.querySelectorAll(selector)]; // return all DOM elements as Array
+    }
+
+    connectedCallback() {
+      this.setSocialFirmaName();
+    }
+
+    setSocialFirmaName() {
+      this.$("#socialFirmaName").forEach(el=>{
+        el.innerText = "Get from URL";
+      });
+    }
+
   }
 );
